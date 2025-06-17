cd2f0b50982172d8f46a34043384f0d51d58be30
Naresh Kumar K <naresh.kumar@mysmitch.com>
2025-06-17 18:40:03 +0530

    Pdf file download for android integrated.

---
+++ composeApp/src/commonMain/kotlin/com/healthanalytics/android/utils/FileDownloader.kt
@@ -0,0 +1,25 @@
+package com.healthanalytics.android.utils
+
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.bodyAsChannel
+import io.ktor.utils.io.*
+import io.ktor.utils.io.core.*
+
+private const val BUFFER_SIZE = 8192
+
+suspend fun downloadFile(client: HttpClient, url: String): ByteArray {
+    return client.get(url).bodyAsChannel().toByteArray()
+}
+
+suspend fun ByteReadChannel.toByteArray(): ByteArray {
+    val buffer = BytePacketBuilder()
+    while (!isClosedForRead) {
+        val packet = readRemaining(BUFFER_SIZE.toLong())
+        buffer.writePacket(packet)
+    }
+    return buffer.build().readBytes()
+}
+
+expect fun saveFile(fileName: String, bytes: ByteArray, context: Any? = null): String 
