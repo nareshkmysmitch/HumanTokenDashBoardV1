aea2858304da0bec093471a2e4b5a19a9dd94a01
Naresh Kumar K <naresh.kumar@mysmitch.com>
2025-06-19 18:59:59 +0530

    csv file reader issue fix.

--- composeApp/src/commonMain/kotlin/com/healthanalytics/android/App.kt
+++ composeApp/src/commonMain/kotlin/com/healthanalytics/android/App.kt
@@ -4,13 +4,10 @@ import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import com.healthanalytics.android.presentation.navigation.MainScreen
 import com.healthanalytics.android.presentation.theme.AppTheme
-import com.healthanalytics.android.presentation.screens.health.HealthDataViewModel
-import org.koin.compose.koinInject
 
 @Composable
 fun App() {
     AppTheme {
-        val healthDataViewModel: HealthDataViewModel = koinInject()
         Navigator(MainScreen())
     }
 }
