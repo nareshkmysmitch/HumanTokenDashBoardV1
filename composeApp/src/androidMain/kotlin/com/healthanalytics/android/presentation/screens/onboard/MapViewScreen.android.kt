b7d7287187a38784e4507293a7f2f81ec6c598de
rethinavelsmitch <rethinavel.natarajan@mysmitch.com>
2025-06-20 16:19:00 +0530

    mapview screen implementations

--- composeApp/src/androidMain/kotlin/com/healthanalytics/android/presentation/screens/onboard/MapViewScreen.android.kt
+++ composeApp/src/androidMain/kotlin/com/healthanalytics/android/presentation/screens/onboard/MapViewScreen.android.kt
@@ -1,38 +1,91 @@
 package com.healthanalytics.android.presentation.screens.onboard
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapEffect
+import com.google.maps.android.compose.MapsComposeExperimentalApi
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
 import com.google.android.gms.maps.model.LatLng as GmsLatLng
 
 actual class LatLng actual constructor(actual val latitude: Double, actual val longitude: Double)
 
+
+@OptIn(MapsComposeExperimentalApi::class)
 @Composable
 actual fun MapView(
-    modifier: Modifier,
-    markerPosition: LatLng,
-    onMarkerDragged: (LatLng) -> Unit
+    modifier: Modifier, markerPosition: LatLng, onMarkerDragged: (LatLng) -> Unit
 ) {
     val cameraPositionState = rememberCameraPositionState {
-        position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(
-            GmsLatLng(markerPosition.latitude, markerPosition.longitude),
-            15f
+        position = CameraPosition.fromLatLngZoom(
+            GmsLatLng(markerPosition.latitude, markerPosition.longitude), 14f
         )
     }
+
+    val markerState =
+        rememberMarkerState(position = GmsLatLng(markerPosition.latitude, markerPosition.longitude))
+
     GoogleMap(
-        modifier = modifier,
-        cameraPositionState = cameraPositionState
+        modifier = Modifier.fillMaxSize(), cameraPositionState = cameraPositionState
     ) {
         Marker(
-            state = com.google.maps.android.compose.rememberMarkerState(
-                position = GmsLatLng(markerPosition.latitude, markerPosition.longitude)
-            ),
-            draggable = true,
-            onDragEnd = {
-                onMarkerDragged(LatLng(it.latitude, it.longitude))
-            }
+            state = markerState, draggable = true
         )
+
+        MapEffect(markerState) { map ->
+            map.setOnMarkerDragListener(object : GoogleMap.OnMarkerDragListener {
+                override fun onMarkerDragStart(p0: Marker) {}
+
+                override fun onMarkerDrag(p0: Marker) {}
+
+                override fun onMarkerDragEnd(p0: Marker) {
+                    onMarkerDragged(LatLng(p0.position.latitude, p0.position.longitude))
+                }
+            })
+        }
     }
-} 
+}
+
+
+//@Composable
+//actual fun MapView(
+//    modifier: Modifier, markerPosition: LatLng, onMarkerDragged: (LatLng) -> Unit
+//) {
+//    val cameraPositionState = rememberCameraPositionState {
+//        position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(
+//            GmsLatLng(markerPosition.latitude, markerPosition.longitude), 15f
+//        )
+//    }
+//
+//
+//    GoogleMap(
+//        modifier = modifier,
+//        cameraPositionState = cameraPositionState,
+//        onMarkerDragEnd = { marker ->
+//            onMarkerDragged(
+//                LatLng(marker.position.latitude, marker.position.longitude)
+//            )
+//        }
+//    ) {
+//        Marker(
+//            state = rememberMarkerState(
+//                position = GmsLatLng(markerPosition.latitude, markerPosition.longitude)
+//            ),
+//            draggable = true
+//        )
+//    }
+//
+//    GoogleMap(
+//        modifier = modifier,
+//        cameraPositionState = cameraPositionState,
+//
+//        ) {
+//
+//    }
+//}
